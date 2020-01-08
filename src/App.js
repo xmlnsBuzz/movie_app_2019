948d2c551d9042d2e8a2a50d32ed25e313d75ad6
xmlnsBuzz <html5conq@gmail.com>
2020-01-08 14:19:40 +0900

    map Recap - 1

--- src/App.js
+++ src/App.js
@@ -11,31 +11,36 @@ function Food({ name, picture }) {
 const foodILike = [
    {
       name: 'food1',
-      image: 'https://picsum.photos/id/1/200/300'
+      image: 'https://picsum.photos/id/1/100/100'
    },
    {
       name: 'food2',
-      image: 'https://picsum.photos/id/2/200/300'
+      image: 'https://picsum.photos/id/2/100/100'
    },
    {
       name: 'food3',
-      image: 'https://picsum.photos/id/3/200/300'
+      image: 'https://picsum.photos/id/3/100/100'
    },
    {
       name: 'food4',
-      image: 'https://picsum.photos/id/4/200/300'
+      image: 'https://picsum.photos/id/4/100/100'
    },
    {
       name: 'food5',
-      image: 'https://picsum.photos/id/5/200/300'
-   },
-
+      image: 'https://picsum.photos/id/5/100/100'
+   }
 ]
+
+function renderFood(dish) {
+   return (
+      <Food name={dish.name} picture={dish.image} />
+   )
+}
+// 위와 같이 Food component 자체를 function에 담아 아래 App component를 단순화 시킬 수 있다.
 
 function App() {
    return (
       <div>
-         <h1>hello</h1>
          {foodILike.map(dish => (
             <Food name={dish.name} picture={dish.image} />
          ))}
