ee0b0b4a11995802d303c3e9c1cf041614b69c1a
xmlnsBuzz <html5conq@gmail.com>
2020-01-08 21:20:42 +0900

    render() function에 의해 render되는 순서를 clg로 알아봄.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class App extends React.Component {
+   constructor(props) {
+      super(props)
+      console.log('hello')
+   }
    // 1 state를 선언한 아래 부분이 state다. 아래에서 부터 line 21는 외워라.
    state = {
       count: 0
@@ -23,7 +27,16 @@ class App extends React.Component {
    // add, minus에 값을 직접 지정하면 'Do not mutate state directly. Use setState()  react/no-direct-mutation-state'처럼 값을 직접 지정하지 말라는 경고가 뜬다.
    // state 속의 값이 바뀌면 state가 update되고, update 된 결과로 state를 refresh시켜 re-render가 발생한다.
 
+
+   componentDidMount() {
+      console.log('component rendered');
+   }
+
+   componentDidUpdate() {
+      console.log('I just updated');
+   }
    render() {
+      console.log('render')
       return (
          <div>
             <h1>The number is : {this.state.count}. </h1>
