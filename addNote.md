ca02d29bcc7c6ef9bff370ce0f256b06b258df66
xmlnsBuzz <html5conq@gmail.com>
2020-01-08 19:14:58 +0900

    #3 0 Class Components and State 들어가기 전

---
+++ addNote.md
@@ -0,0 +1,6 @@
+# Noting
+
+## npx create-react-app [folderName] 이후 설치할 것들.
+
+### prop-types 설치
+>npm i prop-types
--- src/App.js
+++ src/App.js
@@ -1,48 +1,67 @@
 import React from 'react';
-function Food({ name, picture }) {
+import PropTypes from 'prop-types';
+
+const foodILike = [
+   {
+      id: 1,
+      name: 'food1',
+      image: 'https://picsum.photos/id/1/100/100',
+      rating: 5
+   },
+   {
+      id: 2,
+      name: 'food2',
+      image: 'https://picsum.photos/id/2/100/100',
+      rating: 4.9
+   },
+   {
+      id: 3,
+      name: 'food3',
+      image: 'https://picsum.photos/id/3/100/100',
+      rating: 4.8
+   },
+   {
+      id: 4,
+      name: 'food4',
+      image: 'https://picsum.photos/id/4/100/100',
+      rating: 5.5
+   },
+   {
+      id: 5,
+      name: 'food5',
+      image: 'https://picsum.photos/id/5/100/100',
+      rating: 4.7
+   }
+];
+// id: 는 기본적으로 react 내부에서 사용하기 위하여 만든다.
+
+function Food({ name, picture, rating }) {
    return (
       <React.Fragment>
-         <h3>I like {name}.</h3>
-         <img src={picture} />
+         <h3>I like {name}. </h3>
+         <h4>{rating}/5.0</h4>
+         <img src={picture} alt={name} />
       </React.Fragment>
    )
 }
 
-const foodILike = [
-   {
-      name: 'food1',
-      image: 'https://picsum.photos/id/1/100/100'
-   },
-   {
-      name: 'food2',
-      image: 'https://picsum.photos/id/2/100/100'
-   },
-   {
-      name: 'food3',
-      image: 'https://picsum.photos/id/3/100/100'
-   },
-   {
-      name: 'food4',
-      image: 'https://picsum.photos/id/4/100/100'
-   },
-   {
-      name: 'food5',
-      image: 'https://picsum.photos/id/5/100/100'
-   }
-]
-
-function renderFood(dish) {
-   return (
-      <Food name={dish.name} picture={dish.image} />
-   )
+Food.propTypes = {
+   name: PropTypes.string.isRequired,
+   picture: PropTypes.string.isRequired,
+   rating: PropTypes.number.isRequired,
 }
-// 위와 같이 Food component 자체를 function에 담아 아래 App component를 단순화 시킬 수 있다.
 
 function App() {
    return (
       <div>
+         <h1 style={{ textAlign: 'center' }}>Favorite Food</h1>
          {foodILike.map(dish => (
-            <Food name={dish.name} picture={dish.image} />
+            <Food
+               key={dish.id}
+               name={dish.name}
+               picture={dish.image}
+               rating={dish.rating}
+            />
          ))}
       </div>
    )
