d7b1d34ce58e6d31ceb0811df0b0f40bbd7c2046
xmlnsBuzz <html5conq@gmail.com>
2020-01-08 11:29:34 +0900

    props에 대한 설명

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+function Potato({ fav }) {
+   console.log(fav)
+   return <h3>I {fav}love potato.</h3>
+}
+
+/* 
+function args에 모든 props를 넣고 싶을 때는 fucntion(props)
+               특정 props를   "     "     function({propsNames,}*) 
+ */
+
+/* 
+react          html
+component = element
+props       = attributes
+value       = value
+*/
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div>
+         <h1>hello</h1>
+         {/* <Potato 
+            fav="kimchi"
+            something={true}
+            arr={['hello', 1, 2, 3, 4, true]}
+         /> */}
+         <Potato fav="food" />
+         <Potato fav="food2" />
+         <Potato fav="food3" />
+         <Potato fav="food4" />
+      </div>
+   )
 }
 
 export default App;
