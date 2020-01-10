19499f18a160cc9df68df2ff862dc3c851f79abc
xmlnsBuzz <html5conq@gmail.com>
2020-01-10 14:40:44 +0900

    Updates

---
+++ precache-manifest.25d696dbf3a4c028e192f6419116ab6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b8a4c379d12fdd68875531c212bb90",
+    "url": "/xmlnsBuzz/movie_app_2019.git/index.html"
+  },
+  {
+    "revision": "17f2accf55d5c8de5bae",
+    "url": "/xmlnsBuzz/movie_app_2019.git/static/css/main.4ea18140.chunk.css"
+  },
+  {
+    "revision": "4a34198eef49fa1bcfdd",
+    "url": "/xmlnsBuzz/movie_app_2019.git/static/js/2.17071429.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/xmlnsBuzz/movie_app_2019.git/static/js/2.17071429.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17f2accf55d5c8de5bae",
+    "url": "/xmlnsBuzz/movie_app_2019.git/static/js/main.28e9b641.chunk.js"
+  },
+  {
+    "revision": "dd0bff9cb34927093b74",
+    "url": "/xmlnsBuzz/movie_app_2019.git/static/js/runtime-main.f9e2204e.js"
+  }
+]);
