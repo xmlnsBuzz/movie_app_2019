8e1025b023fefa10c2169432e43161fc8a2adcf6
xmlnsBuzz <html5conq@gmail.com>
2020-01-10 14:04:01 +0900

    after css styling

--- addNote.md
+++ addNote.md
@@ -4,3 +4,9 @@
 
 ### prop-types 설치
 >npm i prop-types
+
+### axios 설치
+remote data를 가져오기 위해 설치한다. App.js 참조
+"https://yts-proxy.now.sh/list_movies.json"
+
+>npm i axios
--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
-   constructor(props) {
-      super(props)
-      console.log('hello')
-   }
-   // 1 state를 선언한 아래 부분이 state다. 아래에서 부터 line 21는 외워라.
    state = {
-      count: 0
+      isLoading: true,
+      movies: []
    };
-
-   add = () => {
-      // this.setState(current => ({ count: this.state.count + 1 }));
-      this.setState(current => ({ count: current.count + 1 }));
-
-   };
-   // this.state.count 에서 state는 위의 1 아래 선언된 부분의 count 이다.
-   
-   minus = () => {
-      // this.setState(current => ({ count: this.state.count - 1 }));
-      this.setState(current => ({ count: current.count - 1 }));
-   };
-   // current keyword?를 사용하여 this.state를 'current'로 대체시킬 수 있다.
-
-   // add, minus에 값을 직접 지정하면 'Do not mutate state directly. Use setState()  react/no-direct-mutation-state'처럼 값을 직접 지정하지 말라는 경고가 뜬다.
-   // state 속의 값이 바뀌면 state가 update되고, update 된 결과로 state를 refresh시켜 re-render가 발생한다.
-
+   getMovies = async () => {
+      const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+      this.setState({ movies, isLoading: false });
+   }
 
    componentDidMount() {
-      console.log('component rendered');
-   }
-
-   componentDidUpdate() {
-      console.log('I just updated');
+      this.getMovies();
    }
    render() {
-      console.log('render')
+      const { isLoading, movies } = this.state;
       return (
-         <div>
-            <h1>The number is : {this.state.count}. </h1>
-            <button onClick={this.add}>Add</button>
-            <button onClick={this.minus}>Minus</button>
-         </div>
 
+         <section className="container">
+            {isLoading
+               ? (
+                  <div className="loader">
+                     <span className="loader_text">"Loading..."</span>
+                  </div>
+               ) : (
+                  <div className="movies">
+                     {movies.map(movie => (
+                        <Movie
+                           key={movie.id}
+                           id={movie.id}
+                           year={movie.year}
+                           title={movie.title}
+                           summary={movie.summary}
+                           poster={movie.medium_cover_image}
+                           genres={movie.genres}
+                        />
+                     ))}
+                  </div>
+               )}
+         </section>
       )
    }
 }
