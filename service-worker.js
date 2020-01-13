c752682296e2642fddbfc6299c58f40d5ef560ea
xmlnsBuzz <html5conq@gmail.com>
2020-01-13 10:52:10 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2019/precache-manifest.40924f7e66afe9b5c7104642f7a732f6.js"
+  "/movie_app_2019/precache-manifest.a00cef5ad0e695069a6a2dcd5fdd14b2.js"
 );
 
 self.addEventListener('message', (event) => {
